46dbc6c65f9a252ca5b742d79bf10f09f6d99320
Adrian Laptop <adrian.filipescu@gmail.com>
2012-10-31 00:03:36 +0200

    Work week1 first draft.

--- Week1/week1_rpsls.py
+++ Week1/week1_rpsls.py
@@ -18,6 +18,31 @@ def number_to_name(number):
     
     # convert number to a name using if/elif/else
     # don't forget to return the result!
+     # fill in your code below
+
+   
+    if number == 0:
+        outName = "rock"
+    elif number == 1:
+        outName = "Spock"
+    elif number == 2:
+        outName = "paper"
+    elif number == 3:
+        outName = "lizard"
+    elif number == 4:
+        outName = "scissors"
+    else:
+        outName = "Error Input"
+        
+    return outName
+    
+print "convert to number  = ", number_to_name(3)  
+
+print "-4 mod 5", -4 %5
+print "4 mod 5", 4 %5
+
+    
+        
 
     
 def name_to_number(name):
@@ -25,22 +50,55 @@ def name_to_number(name):
 
     # convert name to number using if/elif/else
     # don't forget to return the result!
+    if name == "rock":
+        outName = 0
+    elif name == "Spock":
+        outName = 1
+    elif name == "paper":
+        outName = 2
+    elif name == "lizard":
+        outName = 3
+    elif name == "scissors":
+        outName = 4
+    else:
+        outName = "Error Input"
+        
+    return outName
+    
+    
+print "convert to name  = ", name_to_number("scissors")
 
+import random
 
 def rpsls(name): 
     # fill in your code below
 
     # convert name to player_number using name_to_number
+    player_number = name_to_number(name)
 
     # compute random guess for comp_number using random.randrange()
-
+    comp_number = random.randrange(0,5)
     # compute difference of player_number and comp_number modulo five
-
-	# use if/elif/else to determine winner
-
-    # convert comp_number to name using number_to_name
+    deltaModulo = (player_number - comp_number) % 5
+    # use if/elif/else to determine winner
+    if deltaModulo == 1 or deltaModulo == 2:
+        winner = "player"
+    elif deltaModulo == 3 or deltaModulo == 4:
+        winner = "computer"
+    elif deltaModulo == 0:
+        winner = "draw"
+    else:
+        winner = "error"
     
+    
+    # convert comp_number to name using number_to_name
+    comp_name = number_to_name(comp_number)
     # print results
+    
+    print"\n ==== ", deltaModulo
+    print "player_name = ", name
+    print "comp_name = ", comp_name
+    print "winner = ", winner
 
     
 # test your code
@@ -51,5 +109,3 @@ rpsls("lizard")
 rpsls("scissors")
 
 # always remember to check your completed program against the grading rubric
-
-
